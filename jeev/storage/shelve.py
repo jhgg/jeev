a3e797fd40bc9e77f0a3652311721e51ba0bb7bb
Jake <jh@tsl.io>
2014-09-03 01:29:55 -0400

    Shelve already adds a prefix. Whoops!

--- jeev/storage/shelve.py
+++ jeev/storage/shelve.py
@@ -10,13 +10,9 @@ class ShelveStore(object):
         self._jeev = jeev
         self._opts = opts
         self._module_data_path = opts['shelve_data_path']
-        self._file_suffix = opts.get('shelve_file_suffix', 'db').lstrip('.')
-
-    def _storage_filename_for(self, module_name):
-        return '%s.%s' % (os.path.join(os.path.join(self._module_data_path, module_name)), self._file_suffix)
 
     def get_data_for_module_name(self, module_name):
-        return shelve_open(self._storage_filename_for(module_name), writeback=True)
+        return shelve_open(os.path.join(self._module_data_path, module_name), writeback=True)
 
     def start(self):
         if not os.path.exists(self._module_data_path):
